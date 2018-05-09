905c265fcc505e254a782407518388e2f5f27307
Shashaank Krishnatray <shashaank.krishnatray@gmail.com>
2018-05-09 10:11:02 +0530

    basic layout

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.html',
+  styleUrls: ['./header.scss']
+})
+export class HeaderComponent {
+  title = 'header';
+}
